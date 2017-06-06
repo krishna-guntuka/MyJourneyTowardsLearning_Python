fb41f750c48f0d99121f95a7b2ec08f0c2ae9eac
krishna <guntukakrishnareddy@gmail.com>
2017-06-05 23:00:36 -0500

    Game progress One

--- guessNumber.py
+++ guessNumber.py
@@ -1,3 +1,16 @@
-print "Game coming soon".
+print "Game coming soon"
 
+jackPotNumber = 3
 
+def guessNum():
+	inputNuber = input("Please enter the jackpot number between 1-10")
+        
+        if jackPotNumber == inputNuber:
+	        result = "You have won 100$"
+	else:
+	        result = "You lost"
+          
+	return result
+
+print guessNum()
+   
