cd9dda1a0ec02af2460e0358a1b143b4aba7e4cc
krishna <guntukakrishnareddy@gmail.com>
2017-06-07 21:00:40 -0500

    second day

--- guessNumber.py
+++ guessNumber.py
@@ -1,22 +1,3 @@
-print "Game coming soon"
-<<<<<<< HEAD
-=======
-
-
-jackpotNumber = 3
-
-def guessNum():
-	inputNum = input("please input number")
-	if jackpotNumber == inputNum:
-		result = "You won jackpot"
-	else:
-		result = "You lost buddy"
-		
-	return result
-
-print guessNum()
->>>>>>> 239f5c4609314a6e40b738bd9abedc67ad6fc5f6
-
 jackPotNumber = 3
 
 def guessNum():
